6e63cd6db262a67d3591b2996faabc1dab4afce7
AndreyT <shax0491@gmail.com>
2024-02-20 14:10:34 +0300

    writing code for Toy

--- Toy.java
+++ Toy.java
@@ -1,5 +1,19 @@
-package JavaToyFinals;
-
-public class Toy {
-    
+public class Toy implements Lotable {
+    int id;
+    String name;
+    public Toy(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    @Override
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    @Override
+    public String toString() {
+        return "Игрушка №" + id + ". " + name;
+    }
 }
